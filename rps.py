fed1f08ccc3d13de157928cffeab1835bd72363f
x0nidma <amirhoseinvalie@yahoo.com>
2018-10-10 15:24:43 +0330

    this is a first project in github

---
+++ rps.py
@@ -0,0 +1,40 @@
+print("hello welcome to playing rock paper scissors")
+print("Well, you can try your luck three times ")
+import random
+x=["rock","paper","scissors"]
+rand=random.choice(x)
+user = input("please enter (rock,paper,scissors) ")
+def condition():
+    if rand == "rock" and user == "rock":
+        print("We got equal",)
+        print("my guess was",rand)
+    elif rand == "rock" and user=="paper":
+        print("you have won",)
+        print(rand)
+    elif rand == "paper" and user=="rock":
+        print("i won")
+        print("my guess was ",rand)
+    elif rand == "rock" and user == "scissors":
+        print("i won")
+        print("my guess was ",rand)
+    elif rand == "scissors" and user == "rock":
+        print("you have won")
+        print("my guess was ",rand)
+    elif rand == "paper" and user == "paper":
+        print("We got equal")
+        print("my guess was ",rand)
+    elif rand=="paper" and user == "scissors":
+        print("you have won")
+        print("my guess was ",rand)
+    elif rand == "scissors" and user == "paper":
+        print("i won")
+        print("my guess was ",rand)
+    else:
+        print("invalid string")
+condition()
+rand=random.choice(x)
+user=input("ok please einter try again (rock,paper,scissors)")
+condition()
+rand=random.choice(x)
+user=input("ok please einter try again (rock,paper,scissors)")
+condition()
